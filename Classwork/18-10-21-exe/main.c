c39e963d5d93d60161a24546f655b76ca4538dd5
Kaloyan Doychonov <kokolia1070@gmail.com>
2021-10-18 17:29:25 +0300

    [ADD] EX2 & EX3

--- Classwork/18-10-21-exe/main.c
+++ Classwork/18-10-21-exe/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// EXERCISES
+
+// EX1
+
 unsigned int crossesAt(unsigned int x1, unsigned int v1, unsigned int x2, unsigned int v2){
     unsigned int v = v1 - v2;
     unsigned int x = x2 - x1;
@@ -24,13 +28,33 @@ bool crossesButBetter(unsigned int x1, unsigned int v1, unsigned int x2, unsigne
     return (!((size_t)(x1 - v1) % (size_t)(x2 - v2))) || (x1 == x2);
 }
 
+// EX2
+
+bool is_odd(int a){
+    return a & 1;
+}
+
+bool sum_of_odd_is_odd(int a){
+    int sum = 0;
+    for(int i = 0; i < sizeof(int); ++i)
+        sum += is_odd(a>>i*8);
+    return is_odd(sum);
+}
+
 int main(){
-    //printf("Crosses at: %d\n", crossesAt(59, 37, 533, 2));
+/*     printf("Crosses at: %d\n", crossesAt(59, 37, 533, 2));
     printf("Crosses hmmm: %s\n", crosses(59, 37, 533, 2) ? "True" : "False");
     printf("Crosses hmmm: %s\n", crosses(1, 3, 4, 2) ? "True" : "False");
     printf("Crosses hmmm but better: %s\n", crossesButBetter(59, 37, 533, 2) ? "True" : "False");
     printf("Crosses hmmm but better: %s\n", crossesButBetter(1, 3, 4, 2) ? "True" : "False");
-    printf("Crosses hmmm but better: %s\n", crossesButBetter(1, 1, 1, 1) ? "True" : "False");
+    printf("Crosses hmmm but better: %s\n", crossesButBetter(1, 1, 1, 1) ? "True" : "False"); */
+
+    // Exercise 2
+
+    //printf("%d\n", is_odd(5));
+
+    // Exercise 3
+    printf("%d\n", sum_of_odd_is_odd(556));
 
     return 0;
 }
