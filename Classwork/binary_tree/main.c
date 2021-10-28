d86fa5362316db481f8da04c23e3413e8fc1d5b2
Kaloyan Doychinov <kokolia1070@gmail.com>
2021-10-28 20:01:50 +0300

    [UPDATE] BiTree

--- Classwork/binary_tree/main.c
+++ Classwork/binary_tree/main.c
@@ -4,14 +4,16 @@
 typedef struct node_t{
     int value;
     struct node_t* left, *right;
+    unsigned char level;
 } node_t;
 
-node_t* add(node_t* root, int value){
+node_t* add(node_t* root, int value, unsigned char level){
     if(!root){
         node_t* new_node = (node_t*)malloc(sizeof(node_t));
         new_node->left = NULL;
         new_node->right = NULL;
         new_node->value = value;
+        new_node->left = level + 1;
         return new_node;
     }
 
@@ -29,9 +31,34 @@ void printBT(node_t* root){
     printf("%d\n", root->value);
     size_t pos = 0;
     while(){
-        
+
     }
     printf("\n");
+}
+
+unsigned char _height(node_t* node, unsigned char level){
+    if(node == NULL)
+        return level;
+
+    if(!node->right, level + 1)
+        unsigned char r =  _height(node->right, level +1)
+
+    if(!node->lefr, level + 1)
+        unsigned char l =  _height(node->left, level +1)
+
+    return r > l ? r : l;
+}
+
+unsigned char height(node_t* root){
+    return _height(root, 0);
+}
+
+void printCBT(node_t* node){
+    if(!node->left)
+        printf("%d ", node->left->value);
+
+    if(!node->right)
+        printf("%d ", node->right->value);
 }
 
 void kill_list(node_t* root){
@@ -39,11 +66,11 @@ void kill_list(node_t* root){
 }
 
 int main(){
-    node_t* bin = add(NULL, 15);
-    bin = add(bin, 11);
-    bin = add(bin, 8);
-    bin = add(bin, -2);
-    bin = add(bin, 20);
+    node_t* bin = add(NULL, 15, -1);
+    bin = add(bin, 11, -1);
+    bin = add(bin, 8, -1);
+    bin = add(bin, -2, -1);
+    bin = add(bin, 20), -1);
     printBT(bin);
     return 0;
 }
