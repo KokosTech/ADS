749a46a3698f1d742a09f046ac4ee0e76c1deb0d
Kaloyan Doychinov <kokolia1070@gmail.com>
2021-11-06 15:59:32 +0200

    [UPDATE] XOR HW

--- Homework/opt/xor_list/main.c
+++ Homework/opt/xor_list/main.c
@@ -1,115 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct list_t{
-    struct list_t* p;
-    int data;
-} list_t;
+typedef struct node{
+    char data;
+    struct node* xpn;
+} Node;
 
 void* xor (void* a, void* b) {
-    return (void*)((long unsigned int)a ^ (long unsigned int)b);
+	return (void*)((uintptr_t)a ^ (uintptr_t)b);
 }
 
-list_t* x_remove(list_t* list){
+void push(Node** head, char value){
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = value;
+    node->xpn = *head;
+
+    if(*head == NULL){
+        *head = (Node*)malloc(sizeof(Node));
+    }
+
+    if(*head == !NULL)
+        (*head)->xpn = (uintptr_t)(*head)->xpn ^ (uintptr_t)node;
+
+    *head = node;
+
+}
+
+Node* x_remove(Node* list){
     if (list == NULL)
         return NULL;
 
-    list_t *next_item = list->p;
+    Node *next_item = list->xpn;
     free(list);
 
     if (next_item == NULL)
         return NULL;
     
-    next_item->p = xor(list, next_item->p);
+    next_item->xpn = xor(list, next_item->xpn);
     
     return next_item;
 }
 
-list_t* destroy_list(list_t *list){
-    //for(list_t *curr_item=list; curr_item!=NULL;curr_item=x_remove(curr_item));
+Node* destroy_list(Node *list){
     for (; list != NULL; list = x_remove(list));
-
     return NULL;
 }
 
-list_t* add(list_t* list, int data){
-    list_t* item = malloc(sizeof(list_t));
-    item->data = data;
-
-    /*if(!list->p)
-    list->p = list->p ^ item;
-
-    else
-    list->p = (list->p ^ item) ^ list->p;*/
-
-    //item -> p = NULL ^ list;
-    item->p = list;
-    
-    if(list == NULL){
-        list->p = NULL;
-    }
-    // list->p = item ^ list->p;
-
-    if (list != NULL)
-        list->p = xor(item, list->p);
-    
-    return item;
-}
-
-void print_list(list_t* list){
-    list_t* prev = NULL;
-    list_t* tmp = list;
+void print_list(Node* list){
+    Node* prev = NULL;
+    Node* tmp = list;
 
     while (tmp != NULL){
-        printf("tmp=%p data=%d p=%p\n", tmp, tmp->data, tmp->p);
+        printf("tmp=%p data=%d p=%p\n", tmp, tmp->data, tmp->xpn);
 
-        list_t* tmp2 = tmp;
-        //tmp = tmp->p ^ prev;
-        tmp = xor(tmp->p, prev);
+        Node* tmp2 = tmp;
+        tmp = xor(tmp->xpn, prev);
         prev = tmp2;
     }
 
     puts("");
 }
 
-int find_value(list_t* list, int value_to_find){
+char find(Node* head, char __data){
+    Node* current, *prev, *next;
+    current = head;
+    prev = NULL;
+
+    while(current && current->data != __data){
+        next = (uintptr_t)current->xpn ^ (uintptr_t)prev;
+        prev = current;
+        current = next;
+    }
+
+    return current->data;
+}
+
+/* int find_value(list_t* list, int value_to_find){
     list_t* prev = NULL;
     list_t* tmp = list;
 
     int index = 0;
 
     while(tmp->data != value_to_find){
-        if(list->p == NULL)
+        if(list->xpn == NULL)
             return -1;
         ++index;
         list_t* tmp2 = tmp;
-        tmp = xor(tmp->p, prev);
+        tmp = xor(tmp->xpn, prev);
         prev = tmp2;
     }
 
     return index;
-}
+} */
 
 int main(){
-    //list_t list1= {NULL,5};
-    //list_t* list = &list1;
-    list_t *list = NULL;
-    list = add(list, 5);
+    Node *list = (Node*)malloc(sizeof(Node));;
+    push(list, 5);
 
-    list = add(list, 10);
-    list = add(list, 15);
-    list = add(list, 20);
-    //print_list(list);
+    push(list, 10);
+    push(list, 15);
+    push(list, 20);
 
     list = x_remove(list);
     list = x_remove(list);
     print_list(list);
-    printf("\n%d\n", find_value(list, 5));
-
-    /*for(list_t* tmp = list; tmp != NULL; tmp = tmp->next)
-    {
-    printf("%d ", tmp->data);
-    }
-    puts("");*/
+    printf("\n%d\n", find(list, 5));
+    
     return 0;
 }
