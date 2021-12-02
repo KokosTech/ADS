63c82307a90a33ff824644e02cc6545e581a1d51
Kaloyan Doychinov <kokolia1070@gmail.com>
2021-12-02 16:29:51 +0200

    [INIT] Prep

---
+++ Preps/binary_tree/README.md
@@ -0,0 +1,4 @@
+# Binary tree - The finale
+### Everything we studied about the binary tree in jsut one file. What a deal?!
+
+Basically this folder will contain everything we've studied, but re-written by ✨mua✨
---
+++ Preps/binary_tree/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// This is binary search tree, btw;
+
+// To Do:
+// Add / Insert
+// Print
+//   - height function
+//   - size function
+// Delete
+// Search
+// Rotations (left & right)
+// Balancing the tree
+//   - balance the tree - normal functuon
+//   - balance the tree - when inserting / adding a new element
+//   - check if balanced
+// Check if trees / subtrees are identical
+
+
+typedef struct node_t{
+    int data;
+    struct node_t *left, *right; // I'mma finally give up and put *[variable_name], instead of slapping it next to the variable type
+} tree_t;
+
+tree_t *add(tree_t *root, int data){
+    
+}
+
+int main(){
+
+
+    return 0;
+}
